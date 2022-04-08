434d09049c6a9afe203af2c8d9d9b2e29f599ed5
Silvio <20299033628@yopmail.com>
2022-04-08 20:57:02 -0300

    Ejecicio video

---
+++ EjercicioTablero.Negocio/Entidades/Tarea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTablero.Negocio.Entidades
+{
+    public abstract class Tarea
+    {
+        protected int _codigo;
+        protected string _descripcion;
+        protected string _estado;
+        protected DateTime _fechaAlta;
+
+        public int Codigo { get => _codigo; set => _codigo = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public string Estado { get => _estado; set => _estado = value; }
+        public DateTime FechaAlta { get => _fechaAlta; set => _fechaAlta = value; }
+
+        protected Tarea(int codigo, string descripcion, string estado)
+        {
+            Codigo = codigo;
+            Descripcion = descripcion;
+            Estado = estado;
+            FechaAlta = DateTime.Now;
+        }
+
+        protected bool IsFinalizada()
+        {
+            return (Estado == "Finalizada");
+        }
+    }
+}
---
+++ EjercicioTablero.Negocio/Entidades/TareaEspecial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTablero.Negocio.Entidades
+{
+    public class TareaEspecial : Tarea
+    {
+        private double _porcentajeFinalizacion;
+        private int _dificultad;
+        private int _prioridad;
+
+        public double PorcentajeFinalizacion { get => _porcentajeFinalizacion; }
+        public int Dificultad { get => _dificultad;  }
+        public int Prioridad { get => _prioridad;}
+
+        public TareaEspecial(int codigo, string descripcion, string estado, double porcentajeFinalizacion, int dificultad, int prioridad) : base(codigo, descripcion, estado)
+        {
+            _porcentajeFinalizacion = porcentajeFinalizacion;
+            _dificultad = dificultad;
+            _prioridad = prioridad;
+        }
+    }
+}
